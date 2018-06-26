fd66476369fa71929db1df9ba6fcb62d404d59ff
estefania <estefania@monoqi.de>
2018-06-26 17:21:21 +0200

    blogApp 1

---
+++ blogApp/ReduxSimpleStarter/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+          <div>
+
+          </div>
+      </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
